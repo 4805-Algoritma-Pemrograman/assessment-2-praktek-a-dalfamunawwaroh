fa8b1cd2fac345da0cb6e46878e953cf7f865695
Dalfa Munawwarotul Mahmudah <dalfamunawwaroh@gmail.com>
2024-12-12 09:15:41 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -4,11 +4,23 @@ public class Nomor1 {
 
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        int N = input.nextInt();
+        
         /* Pekerjaan anda mulai dari sini */
 
+        System.out.println("Masukkan angka n: ");
+        int N = input.nextInt();
+        for (int i = 0; i < N; i++) {
+        int var2 = N;
+        
+                if (var2 % 2 != 0) {
+                    System.out.println("Angka ganjil dari nol hingga " + var2 + ":");
+                    System.out.println("" + var2 + " ");
+                }
+        }
+           
+        
+        
         /* Pekerjaan anda berakhir sini */
-
         System.out.println(Nomor1.class.getDeclaredMethods().length); 
     }
     
